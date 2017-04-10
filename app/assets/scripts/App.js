ce38898621999a4803a082d43fd10e6ec99fe20d
mats1105 <mats.sommervold@gmail.com>
2017-04-10 21:44:41 +0200

    Started on landing page

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,1 @@
+import StickyHeader from './modules/StickyHeader';
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	entry: {
+		App: "./app/assets/scripts/App.js",
+		Vendor: "./app/assets/scripts/Vendor.js"
+	},
+	output: {
+		path: "/OneDrive/Dokumenter/Jobb/Treebranch/Project/treebranch/app/temp/scripts",	
+		filename: "[name].js"
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	}
+}
